b7d601fd482a6045e77e7be61ce16b818508cff8
Bas Hoekstra <info@bashoekstra.nl>
2020-02-25 15:20:41 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace Guesser
+{
+
+    public partial class MainWindow : Window
+    {
+        private Random ageGuesser = new Random();
+        private int tries = 0;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            guessLabel.Content = Convert.ToString(ageGuesser.Next(5, 110));
+        }
+
+        private void correctButton_Click(object sender, RoutedEventArgs e)
+        {
+            tries = tries + 1;
+            MessageBox.Show($"Aantal pogingen: {tries}");
+            tries = 0;
+            guessLabel.Content = Convert.ToString(ageGuesser.Next(5, 110));
+        }
+
+        private void wrongButton_Click(object sender, RoutedEventArgs e)
+        {
+            guessLabel.Content = Convert.ToString(ageGuesser.Next(5, 110));
+            tries = tries + 1;
+        }
+    }
+}
